8d5d7b96412de9dbe74de89b62aad853c1b676cc
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-01-11 15:46:27 -0600

    Added more functions

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -118,7 +118,7 @@ class MethodsExercises {
   }
 
   public static boolean coinFlip(){
-    int rand = (int)Math.floor(Math.random() * 2);
+    int rand = (int)Math.floor(Math.random() * 100) % 2;
     if(rand == 0){
       return false;
     }else{
@@ -126,20 +126,86 @@ class MethodsExercises {
     }
   }
 
+  public static String randomWalk(int n){
+    if(n < 1){
+      return null;
+    }
+
+    String str = "";
+    for(int i = 0; i < n; i++){
+      if(coinFlip()){
+        str += 1;
+      }else{
+        str += 0;
+      }
+    }
+    return str;
+  }
+
+  public static int countOnes(String str){
+    if(str == null){
+      return -1;
+    }
+
+    char raw[] = str.toCharArray();
+    int ones = 0;
+    for(char ch : raw){
+      if(ch == '1'){
+        ones++;
+      }
+    }
+    return ones;
+  }
+
+  public static int countZeros(String str){
+    if(str == null){
+      return -1;
+    }
+
+    char raw[] = str.toCharArray();
+    int zeros = 0;
+    for(char ch : raw){
+      if(ch == '0'){
+        zeros++;
+      }
+    }
+    return zeros;
+  }
+
   public static void methodsTest2(){
     /*
     Write a method called coinFlip that randomly returns a true or false value.
     */
-    for(int i = 0; i < 100; i++){
+    for(int i = 0; i < 5; i++){
       System.out.println(coinFlip());
     }
     /*
     Write a method named randomWalk that takes in an integer and returns a string of that many random ones and zeros. Example: randomWalk(1) returns a string with one character that is randomly 0 or 1. randomWalk(33) returns a string with 33 random 1s or 0s.
-
+    */
+    for(int i = 0; i < 5; i++){
+      System.out.println(randomWalk(10));
+    }
+    /*
     Write a method named countOnes that takes in a string of ones and zeroes and returns a count of the ones.
-
+    */
+    {
+      String rstr = "";
+      for(int i = 0; i < 5; i++){
+        rstr = randomWalk(10);
+        System.out.format("There are %d ones in %s\n", countOnes(rstr), rstr);
+      }
+    }
+    /*
     Write a method named countZeros that takes in a string of ones and zeroes and returns a count of the 0s.
-
+    */
+    {
+      String rstr = "";
+      for(int i = 0; i < 5; i++){
+        rstr = randomWalk(10);
+        System.out.format("There are %d zeros in %s\n", countZeros(rstr), rstr);
+      }
+    }
+    /*
     Write a method named analyzeRandomWalk that accepts a string containing ones and zeroes and prints out the number of ones and the number of zeroes. This method is void because it does not return anything. Example: analyzeRandomWalk("11000") should print "There are 2 ones and 3 zeroes".
 
     Write a method named longestRun that accepts a string of ones and zeroes and returns an integer that specifies the longest consecutive sequence of either 0s or 1s. Example: longestRun("1001111100") returns 5 because there are 5 ones in a row. longestRun("01001") returns 2 because there are two consecitive zeroes.
